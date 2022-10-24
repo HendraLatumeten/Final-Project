7a2adf8b5c87f22de5c27a6231b8cf684b996e3e
HendraLatumeten <hendrawyt34@gmail.com.com>
2022-10-24 15:14:51 +0700

    update

--- server/src/modules/users/user_controller.go
+++ server/src/modules/users/user_controller.go
@@ -1,13 +1,12 @@
 package users
 
 import (
-	"fmt"
+	"encoding/json"
 
 	"github.com/Lectron-Fazztrack/Final-Project/server/src/database/models"
 	"github.com/Lectron-Fazztrack/Final-Project/server/src/interfaces"
 	"github.com/Lectron-Fazztrack/Final-Project/server/src/libs"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/schema"
 )
 
 type user_ctrl struct {
@@ -19,32 +18,18 @@ func NewCtrl(reps interfaces.UserService) *user_ctrl {
 }
 
 func (re *user_ctrl) Update(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "multipart/form-data")
-	var decoder = schema.NewDecoder()
-	var data models.User
-
 	claim_user, exist := c.Get("email")
-	fmt.Println(claim_user)
 	if !exist {
 		libs.New("claim user is not exist", 400, true)
 		c.Abort()
 	}
-	email := claim_user.(string)
 
-	file, exist := c.Get("image")
-	if !exist {
-		libs.New("file not exist", 400, true)
-		c.Abort()
-	}
-
-	//file upload
-	image := file.(string)
-	data.Image = image
-
-	err := decoder.Decode(&data, c.Request.PostForm)
+	var data models.User
+	err := json.NewDecoder(c.Request.Body).Decode(&data)
 	if err != nil {
 		libs.New(err.Error(), 400, true)
 		c.Abort()
 	}
-	re.svc.Update(&data, email).Send(c)
+
+	re.svc.Update(&data, claim_user.(string)).Send(c)
 }
